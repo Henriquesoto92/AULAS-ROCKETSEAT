636243ec7286395a20b38697960566cd38f6164a
Henrique Soto <96528794+Henriquesoto92@users.noreply.github.com>
2022-02-25 23:04:52 -0300

    aulas

--- 10-O GUIA ESTELAR DE JAVASCRIPT/js/scripts.js
+++ 10-O GUIA ESTELAR DE JAVASCRIPT/js/scripts.js
@@ -1,30 +1,12 @@
-/*
-    manipulando strings e numeros
-*/
-    //transformar uma cadeia de caracteres em elementros de um array
+// for .... in
 
-let techs = ["html", "css", "js"]
+let person = {
+    name: 'john',
+    age: 30,
+    weight: 88.6
+}
 
-// adicionar um item no fim
-techs.push("nodejs")
-
-// adicionar no começo
-techs.unshift("sql")
-
-// remover do fim
-//techs.pop()
-
-// remover do começo
-//techs.shift()
-
-// pegar somente alguns elementos do array
-//console.log(techs.slice(1,1))
-
-// remover 1 ou mais items em qualquer posição do array
-//techs.splice(1,1)
-
-// encontrar a posição de um elemento no array
-let index = techs.indexOf('css')
-techs.splice(index, 2)
-
-console.log(techs)
+for(let property in person) {
+    console.log(property)
+    console.log(person[property])
+}
