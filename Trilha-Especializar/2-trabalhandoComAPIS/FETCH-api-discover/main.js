8a9e82dac6c5939789049c3f44d55d58383d1219
Henriquesoto92 <henriquesoto92@gmail.com>
2022-12-07 21:40:55 -0300

    :construction: study fetch

--- Trilha-Especializar/2-trabalhandoComAPIS/FETCH-api-discover/main.js
+++ Trilha-Especializar/2-trabalhandoComAPIS/FETCH-api-discover/main.js
@@ -1,7 +1,21 @@
-const url = "http://localhost:5500/api";
+const url = "http://localhost:5600/api";
 
 function getUsers() {
   fetch(url)
-    .then()
-    .catch((error) => console(error));
+    .then((response) => response.json())
+    .then((data) => (renderApiResult.textContent = JSON.stringify(data)))
+    .catch((error) => console.error(error));
 }
+function getUser() {
+  fetch(`${url}/1`)
+    .then((response) => response.json())
+    .then((data) => {
+      userName.textContent = data.name;
+      userCity.textContent = data.city;
+      userAvatar.src = data.avatar;
+    })
+    .catch((error) => console.error(error));
+}
+
+getUsers();
+getUser();
